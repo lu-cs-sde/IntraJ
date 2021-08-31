c73cbb783cfce4b2e421496b4e490f9a0234bd3f
IdrissRio <riouakidriss@hotmail.it>
2021-08-31 20:43:02 +0200

    Added PURITY analysis testsfile

---
+++ testfiles/DataFlow/PURE/PURE03/PURE03.java
@@ -0,0 +1,7 @@
+public class PURE03 {
+  Integer y;
+  void m(Integer x, Integer z) {
+    y = x;
+    y = z;
+  }
+}
---
+++ testfiles/DataFlow/PURE/PURE04/PURE04.java
@@ -0,0 +1,7 @@
+public class PURE04 {
+  void m() {
+    Integer x = 0;
+    Integer y = x;
+    Integer z = y;
+  }
+}
